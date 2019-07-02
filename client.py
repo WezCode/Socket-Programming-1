c423aa329420fbe6d92c83033fd8695c42d9815b
unknown <s3719843@student.rmit.edu.au>
2019-07-02 14:34:48 +1000

    Added serialissation (pickling) on a test dictionary object to send to client

--- client.py
+++ client.py
@@ -1,38 +1,38 @@
 import socket
+import pickle
 
 HEADERSIZE = 10
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Usually its on remote machines so cant just use get host name like this
-s.connect((socket.gethostname(), 1235))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Receives a stream of data, how big is a chunk of data that you receive
-
+s.connect((socket.gethostname(), 1235))
 
 while True:
 
-	full_msg = ''
+	full_msg = b''
 	new_msg = True
 	while True:
 	    msg = s.recv(16)
-
 	    if new_msg:
 	    	print(f"new message length: {msg[:HEADERSIZE]}")
-	    	#Strips whitespace automatically in python and will store the length
-	    	# in the int msglen.
-
 	    	# reads in the first 10 characters to extract length of msg
 	    	msglen = int(msg[:HEADERSIZE])
 	    	new_msg = False
 
 	   	
-	    full_msg += msg.decode("utf-8")
+	    full_msg += msg
 
 	    if len(full_msg) - HEADERSIZE == msglen:
 	    	print("full msg received")
 	    	print(full_msg[HEADERSIZE:])
+
+	    	d = pickle.loads(full_msg[HEADERSIZE:])
+	    	print(d)
+
 	    	new_msg = True
-	    	full_msg = ''
+	    	full_msg = b''
 
 	print(full_msg)
 
