76edf1d359418a25adab14ff8a65530b173b1f39
unknown <s3719843@student.rmit.edu.au>
2019-07-02 14:35:32 +1000

    Included deserialisation to understand the dictionary object received from server

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
 import time
+import pickle
+
+
 
 HEADERSIZE = 10
 
@@ -11,15 +14,12 @@ while True:
 	clientsocket, address = s.accept()
 	print(f"Connection from {address} has been established!")
 
-	msg = "Welcome to the server!"
-	msg = f'{len(msg):<{HEADERSIZE}}' + msg
 
-	clientsocket.send(bytes(msg, "utf-8"))
+	d = {1: "Hey", 2: "There"}
+	#This is pickled so is already bytes
+	msg = pickle.dumps(d)
 
-	while True:
-		time.sleep(3)
-		msg = f'The time is {time.time()}'
-		msg = f'{len(msg):<{HEADERSIZE}}' + msg
+	msg = bytes(f'{len(msg):<{HEADERSIZE:}}',"utf-8") + msg
 
-		clientsocket.send(bytes(msg, "utf-8"))
+	clientsocket.send(msg)
 
